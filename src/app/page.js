334a5b997a2bc502f2fa11f41a28e4be304ca28a
sadat khan <sadatcse@gmail.com>
2024-01-18 21:29:52 +0600

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
+import Banner from '@/components/Banner';
+import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
+      <Navbar />
+      <Banner />
       <h1>This is home page</h1>
     </div>
   );
