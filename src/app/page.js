85e3a061db9c5b07689d3be356e0ab9ec5b27cbd
mihir-x <ictlancerx@gmail.com>
2024-01-18 22:41:20 +0600

    [feat](What people say) Added skeliton of what people say section and container component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import SurveyBenefits from '@/components/HomePageComponents/SurveyBenefits';
+import WhatPeopleSay from '@/components/HomePageComponents/WhatPeopleSay';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <h1>This is home page</h1>
+      <SurveyBenefits></SurveyBenefits>
+      <WhatPeopleSay></WhatPeopleSay>
     </div>
   );
 };
---
+++ src/components/HomePageComponents/SurveyBenefits.js
@@ -0,0 +1,11 @@
+
+
+const SurveyBenefits = () => {
+    return (
+        <div>
+            this is survey benefits section 
+        </div>
+    );
+};
+
+export default SurveyBenefits;
